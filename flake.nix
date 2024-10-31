4061a3292fd2820bfe697f8db1e3c9dbe8d726d9
vijo@nixos <vitale.jorgensen@gmail.com>
2024-10-31 15:37:45 +0100

    wip: added catppuccin

--- flake.nix
+++ flake.nix
@@ -1,28 +1,33 @@
 {
-    description = "Vijos home manager configuration";
-    
-    inputs = {
-      nixpkgs.url = "nixpkgs/nixos-24.05";
+  description = "Vijos home manager configuration";
 
-      home-manager = {
-        url = "github:nix-community/home-manager/release-24.05";
-        inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin.url = "github:catppuccin/nix";
+  };
+
+  outputs = {
+    nixpkgs,
+    catppuccin,
+    home-manager,
+    ...
+  }: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    homeConfigurations = {
+      vijoprofile = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+          catppuccin.homeManagerModules.catppuccin
+        ];
       };
     };
-
-    outputs = {nixpkgs, home-manager, ...}:
-        let
-            lib = nixpkgs.lib;
-            system = "x86_64-linux";
-            pkgs = import nixpkgs {inherit system;};
-        in {
-            homeConfigurations ={
-                vijoprofile = home-manager.lib.homeManagerConfiguration{
-                    inherit pkgs;
-                    modules = [./home.nix];
-                };
-            };
-        };
-
-
+  };
 }
--- home.nix
+++ home.nix
@@ -1,51 +1,104 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "vijo";
   home.homeDirectory = "/home/vijo";
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05";
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
- home.packages = with pkgs; [
-      vim
-      tmux
-      zoxide
-      bat
-      fzf
-      oh-my-posh
-      lsd
+  home.packages = with pkgs; [
+    vim
+    tmux
+    zoxide
+    bat
+    fzf
+    oh-my-posh
+    lsd
+    lazygit
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/vijo/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
-
+  # bash
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    historyIgnore = ["ls" "cd" "exit" "ll" "clear" "cl"];
+    shellAliases = {
+      ll = "lsd -la --date relative";
+      c = "clear";
+      l = "ls -CF";
+      ls = "lsd";
+      cd = "z";
+      tree = "lsd --tree";
+      gs = "git status";
+      ".." = "cd ..";
+      lzg = "lazygit";
+    };
+  };
+  # git
+  programs.git = {
+    enable = true;
+    userName = "vijo676";
+    userEmail = "vitale.jorgensen@gmail.com";
+  };
+  # tmux
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    baseIndex = 1;
+    keyMode = "vi";
+    prefix = "C-space";
+    mouse = true;
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+        '';
+      }
+    ];
+    extraConfig = ''
+      bind -Troot C-w switch-client -T VimWindowMovements
+      bind -TVimWindowMovements h select-pane -L
+      bind -TVimWindowMovements j select-pane -D
+      bind -TVimWindowMovements k select-pane -U
+      bind -TVimWindowMovements l select-pane -R
+    '';
+  };
+  # starship
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  # zoxide
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  # lsd colors
+  programs.lsd = {
+    enable = true;
+  };
+  # bat
+  programs.bat = {
+    enable = true;
+  };
+  # fzf
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    defaultOptions = [
+      "--preview 'bat --color=always {}'"
+      "--preview-window '~3'"
+    ];
+  };
+  catppuccin.flavor = "mocha";
+  catppuccin.enable = true;
+  programs.tmux.catppuccin.enable = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
