f8f80e07656a6db255040f6679af5634cfc19247
vijo@nixos <vitale.jorgensen@gmail.com>
2024-11-29 08:37:56 +0100

    feat: moved to unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Vijos home manager configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     catppuccin.url = "github:catppuccin/nix";
--- home.nix
+++ home.nix
@@ -16,6 +16,7 @@
     oh-my-posh
     lsd
     lazygit
+    zed-editor
   ];
   home.file = {
   };
