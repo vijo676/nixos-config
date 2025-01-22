d125feefb8d0c7523db96094d474f294bbe16eb1
vijo@nixos <vitale.jorgensen@gmail.com>
2025-01-22 17:52:07 +0100

    feat: new tmux theme

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -82,23 +82,19 @@
     prefix = "C-space";
     mouse = true;
     plugins = with pkgs.tmuxPlugins; [
+      online-status
       vim-tmux-navigator
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor "mocha"
-          set -g @catppuccin_window_status_style "rounded"
-          set -g @catppuccin_status_background "none"
-          set -g @catppuccin_pane_status_enabled "off"
-          set -g @catppuccin_pane_border_status "off"
-          set -g @catppuccin_status_background "none"
-        '';
-      }
       yank
       resurrect
       continuum
       battery
       cpu
+      prefix-highlight
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+        '';
+      }
     ];
     extraConfig = ''
       # Enable true color
@@ -125,6 +121,53 @@
       bind -TVimWindowMovements j select-pane -D
       bind -TVimWindowMovements k select-pane -U
       bind -TVimWindowMovements l select-pane -R
+
+      # Configure Catppuccin
+      set -g @catppuccin_flavor "macchiato"
+      set -g @catppuccin_status_background "none"
+      set -g @catppuccin_window_status_style "none"
+      set -g @catppuccin_pane_status_enabled "off"
+      set -g @catppuccin_pane_border_status "off"
+
+      # status left look and feel
+      set -g status-left-length 100
+      set -g status-left ""
+      set -ga status-left "#{?client_prefix,#{#[bg=#{@thm_red},fg=#{@thm_bg},bold]  #S },#{#[bg=#{@thm_bg},fg=#{@thm_green}]  #S }}"
+      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
+      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_maroon}]  #{pane_current_command} "
+      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
+      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]#{?window_zoomed_flag,│,}"
+      set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_yellow}]#{?window_zoomed_flag,  zoom ,}"
+
+      # status right look and feel
+      set -g status-right-length 100
+      set -g status-right ""
+      set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]│"
+      set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_blue}] 󰭦 %Y-%m-%d 󰅐 %H:%M "
+
+      # Configure Tmux
+      set -g status-style "bg=#{@thm_bg}"
+      set -g status-justify "absolute-centre"
+
+      # pane border look and feel
+      setw -g pane-border-format ""
+      setw -g pane-active-border-style "bg=#{@thm_bg},fg=#{@thm_overlay_0}"
+      setw -g pane-border-style "bg=#{@thm_bg},fg=#{@thm_surface_0}"
+
+      # window look and feel
+      set -wg automatic-rename on
+      set -g automatic-rename-format "Window"
+
+      set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+      set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
+      set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
+      set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
+      set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
+      set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
+
+      set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+      set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
+
     '';
   };
   # starship
@@ -162,8 +205,8 @@
     enableBashIntegration = true;
     nix-direnv.enable = true;
   };
-  catppuccin.flavor = "mocha";
-  catppuccin.enable = true;
+  # catppuccin.flavor = "mocha";
+  # catppuccin.enable = true;
   catppuccin.tmux.enable = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
