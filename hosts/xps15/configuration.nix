698972047a8014c3253d6aa0a827059e2b989781
vijo676 <vitale.jorgensen@gmail.com>
2025-05-14 16:26:55 +0200

    added stuff

--- hosts/xps15/configuration.nix
+++ hosts/xps15/configuration.nix
@@ -47,6 +47,15 @@
     };
   };
   programs.hyprland.enable = true;
+  env = [
+    "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
+    "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
+    "MOZ_WEBRENDER, 1" # for firefox to run on wayland
+    "XDG_SESSION_TYPE, wayland"
+    "WLR_NO_HARDWARE_CURSORS, 1"
+    "WLR_RENDERER_ALLOW_SOFTWARE, 1"
+    "QT_QPA_PLATFORM, wayland"
+  ];
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -16,28 +16,91 @@ in {
     programs.kitty = {
       enable = true;
     };
-    # Hints the electron apps to use wayland
-    home.sessionVariables.NIXOS_OZONE_WL = "1";
     wayland.windowManager.hyprland.enable = true;
+
     wayland.windowManager.hyprland.settings = {
+      env = [
+        "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
+        "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
+        "MOZ_WEBRENDER, 1" # for firefox to run on wayland
+        "XDG_SESSION_TYPE, wayland"
+        "WLR_NO_HARDWARE_CURSORS, 1"
+        "WLR_RENDERER_ALLOW_SOFTWARE, 1"
+        "QT_QPA_PLATFORM, wayland"
+      ];
+      # Auto start
+      exec-once = [
+        "waybar &"
+        "hyprlock"
+      ];
+
+      input = {
+        kb_layout = "us,dk";
+        kb_options = "grp:alt_caps_toggle";
+        repeat_delay = 300;
+        sensitivity = 0;
+      };
+
+      decoration = {
+        rounding = 0;
+        shadow = {
+          enabled = false;
+          ignore_window = true;
+          offset = "0 2";
+          range = 20;
+          render_power = 3;
+          color = "rgba(00000055)";
+        };
+        blur = {
+          enabled = true;
+          size = 4;
+          passes = 2;
+          brightness = 1;
+          contrast = 1.4;
+          ignore_opacity = true;
+          noise = 0;
+          new_optimizations = true;
+          xray = true;
+        };
+      };
+
+      animations = {
+        enabled = true;
+      };
+
       # Bindings
       "$mod" = "SUPER";
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
       bind =
         [
-          "$mod, Q, killactive"
-          "$mod, B, exec, firefox"
-          "$mod, E, exec, ghostty"
-          "$mod, D, exec, rofi -show drun"
-          "$mod, V, togglefloating"
-          "$mod, F, fullscreen"
+          "$mod, F1, exec, show-keybinds"
+          "$mod, q, killactive"
+          "$mod, b, exec, firefox"
+          "$mod, e, exec, ghostty"
+          "$mod, d, exec, rofi -show drun || pkill rofi"
+          "$mod, v, togglefloating"
+          "$mod, f, fullscreen"
+          "$mod, Escape, exec, swaylock"
+          "ALT, Escape, exec, hyperlock"
 
           # Move focus
-          "$mod SHIFT ,H, movefocus, l"
-          "$mod SHIFT ,L, movefocus, r"
-          "$mod SHIFT ,K, movefocus, u"
-          "$mod SHIFT ,J, movefocus, d"
+          "$mod, h, movefocus, l"
+          "$mod, l, movefocus, r"
+          "$mod, k, movefocus, u"
+          "$mod, j, movefocus, d"
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
 
           # Move active window
+          "$mod SHIFT, h, movewindow, l"
+          "$mod SHIFT, l, movewindow, r"
+          "$mod SHIFT, k, movewindow, u"
+          "$mod SHIFT, j, movewindow, d"
           "$mod SHIFT, left, movewindow, l"
           "$mod SHIFT, right, movewindow, r"
           "$mod SHIFT, up, movewindow, u"
