bd6089103408e0de9678425a053791fce168daf7
vijo676 <vitale.jorgensen@gmail.com>
2025-05-15 22:24:56 +0200

    the nwg-hello still not work

--- hosts/xps15/configuration.nix
+++ hosts/xps15/configuration.nix
@@ -42,8 +42,8 @@
     settings = {
       default_session = {
         # command = "Hyprland -c ${pkgs.nwg-hello}/bin/nwg-hello";
-        # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-        command = "${pkgs.hyprland}/bin/Hyprland -c ${pkgs.nwg-hello}/etc/nwg-hello/hyprland.conf";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        # command = "${pkgs.hyprland}/bin/Hyprland -c ${pkgs.nwg-hello}/etc/nwg-hello/hyprland.conf";
         user = "greeter";
       };
     };
