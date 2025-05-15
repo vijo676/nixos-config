ab2ec9c852f0f1e243a7bc66face0ea5a2d61dfe
vijo676 <vitale.jorgensen@gmail.com>
2025-05-15 22:10:07 +0200

    gg ?

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -122,24 +122,27 @@ in {
     };
 
     # Idle Daemon
-    services.hypridle.enable = true;
-    services.hypridle.settings = {
-    general = {
-        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-        lock_cmd = "hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
-    };
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+            ignore_dbus_inhibit = false;
+            unlock_cmd = "pkill --signal SIGUSR1 hyprlock";
+            lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+            after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+        };
 
-    listener = [
-        {
-        timeout = 300;
-        on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
-        }
-        {
-        timeout = 360;
-        on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-        on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-        }
-    ];
+        listener = [
+            {
+            timeout = 300; # 5 mins
+            on-timeout = "${pkgs.hyprlock}/bin/hyprlock";
+            }
+            {
+            timeout = 360; # 6 mins
+            on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+            on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+            }
+        ];
     };
 
     wayland.windowManager.hyprland.enable = true;
