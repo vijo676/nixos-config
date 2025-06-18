712df25627d2f436ef1c003ecc7af8bd142ac902
vijo676 <vitale.jorgensen@gmail.com>
2025-06-18 23:04:17 +0200

    remove waybar from exec-once to fix double waybar

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -115,7 +115,6 @@ in {
         exec-once = [
           "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
           "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-          "waybar"
           "hyprpaper"
           "hypridle"
           # "hyprpm reload -n"
