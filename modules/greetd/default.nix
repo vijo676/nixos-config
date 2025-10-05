bfabec40949c6a44a6b6277d91a0a383bd57d009
vijo676 <vitale.jorgensen@gmail.com>
2025-10-05 21:47:25 +0200

    chore: more clean up

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -3,9 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        # command = "Hyprland -c ${pkgs.nwg-hello}/bin/nwg-hello";
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-        # command = "${pkgs.hyprland}/bin/Hyprland -c ${pkgs.nwg-hello}/etc/nwg-hello/hyprland.conf";
         user = "greeter";
       };
     };
