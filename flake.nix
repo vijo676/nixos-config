799566f3bdc64429d7d1a75f5ddfeeaeec0bfe4a
vijo@nixos <vitale.jorgensen@gmail.com>
2025-11-28 11:13:40 +0100

    fix: warnings

--- flake.nix
+++ flake.nix
@@ -23,7 +23,6 @@
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.quickshell.follows = "quickshell";
     };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
@@ -40,93 +39,94 @@
     };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      home-manager,
-      lanzaboote,
-      ...
-    }:
-    let
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      username = "vijo";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        config.allowUnsupportedSystem = true;
-      };
-      mkHost = name: {
-        config = ./hosts/${name}/configuration.nix;
-        home = ./hosts/${name}/home.nix;
-      };
-      hosts = {
-        desktop = mkHost "desktop";
-        work = mkHost "work";
-        xps15 = mkHost "xps15";
-      };
-    in
-    {
-      # Dev shells
-      devShells = {
-        x86_64-linux.default = pkgs.mkShell {
-          name = "default";
-          buildInputs = [
-            # nothing special for now
-          ];
-          shellHook = ''
-            echo "💻 Default shell loaded!"
-          '';
-        };
-      };
-      # NixOS configurations
-      nixosConfigurations = lib.mapAttrs (
-        name: host:
-        lib.nixosSystem {
-          inherit system pkgs;
-          specialArgs = {
-            inherit inputs system;
-          };
-          modules = [
-            host.config
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${username} = import host.home;
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-              };
-            }
-          ]
-          ++ lib.optionals (name == "desktop") [
-            lanzaboote.nixosModules.lanzaboote
-            (
-              { lib, ... }:
-              {
-                # Lanzaboote replaces systemd-boot
-                boot.loader.systemd-boot.enable = lib.mkForce false;
-                boot.lanzaboote = {
-                  enable = true;
-                  pkiBundle = "/var/lib/sbctl";
-                };
-              }
-            )
-          ];
-        }
-      ) hosts;
-      # Home-manager configurations
-      homeConfigurations = lib.mapAttrs (
-        name: host:
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            inherit inputs;
-          };
-          modules = [ host.home ];
-        }
-      ) hosts;
-      formatter.${system} = pkgs.alejandra;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    home-manager,
+    lanzaboote,
+    ...
+  }: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    username = "vijo";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      config.allowUnsupportedSystem = true;
     };
+    mkHost = name: {
+      config = ./hosts/${name}/configuration.nix;
+      home = ./hosts/${name}/home.nix;
+    };
+    hosts = {
+      desktop = mkHost "desktop";
+      work = mkHost "work";
+      xps15 = mkHost "xps15";
+    };
+  in {
+    # Dev shells
+    devShells = {
+      x86_64-linux.default = pkgs.mkShell {
+        name = "default";
+        buildInputs = [
+          # nothing special for now
+        ];
+        shellHook = ''
+          echo "💻 Default shell loaded!"
+        '';
+      };
+    };
+    # NixOS configurations
+    nixosConfigurations =
+      lib.mapAttrs (
+        name: host:
+          lib.nixosSystem {
+            inherit system pkgs;
+            specialArgs = {
+              inherit inputs system;
+            };
+            modules =
+              [
+                host.config
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.${username} = import host.home;
+                  home-manager.extraSpecialArgs = {
+                    inherit inputs;
+                  };
+                }
+              ]
+              ++ lib.optionals (name == "desktop") [
+                lanzaboote.nixosModules.lanzaboote
+                (
+                  {lib, ...}: {
+                    # Lanzaboote replaces systemd-boot
+                    boot.loader.systemd-boot.enable = lib.mkForce false;
+                    boot.lanzaboote = {
+                      enable = true;
+                      pkiBundle = "/var/lib/sbctl";
+                    };
+                  }
+                )
+              ];
+          }
+      )
+      hosts;
+    # Home-manager configurations
+    homeConfigurations =
+      lib.mapAttrs (
+        name: host:
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs;
+            };
+            modules = [host.home];
+          }
+      )
+      hosts;
+    formatter.${system} = pkgs.alejandra;
+  };
 }
